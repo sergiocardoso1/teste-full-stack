bf6d4e876b1f64c2fc148d9a5352076b78da02c2
Sérgio Cardoso <sergioifbaiano@hotmail.com>
2023-09-25 13:25:48 -0300

    Projeto Finalizado (Adicionado validacao ao DTO)

--- backend/src/main/java/com/sergiocardoso/backend/dto/ComentarioDTO.java
+++ backend/src/main/java/com/sergiocardoso/backend/dto/ComentarioDTO.java
@@ -1,8 +1,14 @@
 package com.sergiocardoso.backend.dto;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 public class ComentarioDTO {
 	
 	private Long id;
+	@NotEmpty(message = "O campo nome não pode ser vazio")
+	@Length(min = 3, max = 120, message = "O tamanho deve ser entre 3 e 120")
 	private String comentario;
 	private Long cervejaId;
 	
--- backend/src/main/java/com/sergiocardoso/backend/dto/ComentarioNewDTO.java
+++ backend/src/main/java/com/sergiocardoso/backend/dto/ComentarioNewDTO.java
@@ -1,7 +1,13 @@
 package com.sergiocardoso.backend.dto;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 public class ComentarioNewDTO {
 	
+	@NotEmpty(message = "O campo nome não pode ser vazio")
+	@Length(min = 3, max = 120, message = "O tamanho deve ser entre 3 e 120")
 	private String comentario;
 	private Long cervejaId;
 	
